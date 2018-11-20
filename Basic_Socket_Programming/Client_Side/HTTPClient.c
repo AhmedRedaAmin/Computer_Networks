4946a6c7105182a671f4bcfee46103f088cba85c
Ahmed Amin <ahmedredaselim@gmail.com>
2018-11-21 00:58:31 +0200

    Final version of HTTP 1.0 tried and tested with Documentation , along with images to test the functionality

--- Basic_Socket_Programming/Client_Side/HTTPClient.c
+++ Basic_Socket_Programming/Client_Side/HTTPClient.c
@@ -11,15 +11,17 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/sendfile.h>
-#include "DieWithError.c"
+#include "Die_with_error.c"
 
-#define RCVBUFSIZE 1024       /* Size of receive buffer */
+#define RCVBUFSIZE 10240       /* Size of receive buffer */
 
 void handleGetResponse(char* filename, int socket);
 void handlePostResponse_Request(char* filename, int socket, int fifd);
 int isOK(char* msg);
 void sendFileToServer(char *filename, int socket, int filefd);
 void startConnection(char * command, char* command_type, char* file_name, char* host_name, int port_num, int sock);
+void print(char * str);
+int get_file_size(char * filename);
 
 int main(int argc, char *argv[])
 {
@@ -44,8 +46,10 @@ int main(int argc, char *argv[])
     else
         servPort = 8080; /* 8080 is the well-known port for HTTP*/
 
+    print("Before Socket Creation");
     if ((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError(" socket () failed") ;
+    print("After Socket Creation");
 /* Construct the server address structure */
     memset(&servAddr, 0, sizeof(servAddr));
 /* Zero out structure */
@@ -57,66 +61,87 @@ int main(int argc, char *argv[])
     servAddr.sin_port
             = htons(servPort); /* Server port */
 
-    /* Establish the connection to the  server */
-    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
-        DieWithError("connect () failed") ;
-
 
     char * file_path = "commands.txt";
     FILE * fp;
-    char * line = NULL;
+    char * line;
     size_t len = 0;
     ssize_t read;
-    int elements_num_file=0;
-    char * elements_file [5];
     char * pch_file ;
     int port_number = 8080 ;
-    char * command_type = NULL ;
-    char * file_name = NULL ;
-    char * host_name = NULL ;
+    char * command_type;
+    char * file_name = NULL;
+    char * host_name;
+    print("Before Read File Commands");
     fp = fopen(file_path, "r");
     if (fp == NULL)
         DieWithError("Commands File Not Found");
+    print("After Read File Commands");
 
+    /* Establish the connection to the  server */
+    print("Before Socket Connect");
+    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
+        DieWithError("connect () failed") ;
+    print("After Socket Creation");
+
+    print("Start Reading Commands");
     while ((read = getline(&line, &len, fp)) != -1) {
+        print("Start Handling Command");
         char command_arr [strlen(line)];
         strncpy(command_arr , line, strlen(line));
-        pch_file = strtok (command_arr," ");
-        while (pch_file != NULL)
-        {
-            elements_file[elements_num_file]=pch_file;
-            elements_num_file++;
-            pch_file = strtok(NULL, " ");
-        }
-        command_type = elements_file[0];
-        file_name = elements_file[1];
-        host_name = elements_file[2];
-        if (elements_num_file == 4)
-            port_number = atoi(elements_file[3]);
-
+        //printf("The Command is %s\n", line);
+        command_type = strtok (command_arr," ");
+        file_name = strtok (NULL," ");
+        host_name = strtok (NULL," ");
+        char * pn = strtok(NULL, " ");
+        if (pn != NULL)
+            port_number = atoi(pn);
+        print("End Handling Command");
+        print("Start Connection");
         startConnection(line, command_type,file_name,host_name,port_number,sock);
+        print("End Connection");
+        print("Close Socket");
     }
+    print("End Reading Commands");
     fclose(fp);
+
+    /*startConnection("GET fileNew2.txt 127.0.0.1\\r\\n\r", "GET","fileNew2.txt","127.0.0.1",8080,sock);
+    startConnection("GET fileNew2.txt 127.0.0.1\\r\\n\r", "GET","fileNew2.txt","127.0.0.1",8080,sock);
+    startConnection("GET fileNew2.txt 127.0.0.1\\r\\n\r", "GET","fileNew2.txt","127.0.0.1",8080,sock);
+    startConnection("GET fileNew2.txt 127.0.0.1\\r\\n\r", "GET","fileNew2.txt","127.0.0.1",8080,sock);
+    startConnection("GET fileNew2.txt 127.0.0.1\\r\\n\r", "GET","fileNew2.txt","127.0.0.1",8080,sock);
+    startConnection("POST fileNew.txt 127.0.0.1\\r\\n\r", "POST","fileNew.txt","127.0.0.1",8080,sock);
+    startConnection("GET fileNew2.txt 127.0.0.1\\r\\n\r", "GET","fileNew2.txt","127.0.0.1",8080,sock);*/
     close(sock);
     exit(0);
 }
 
 void startConnection(char * command, char* command_type, char* file_name, char* host_name, int port_num, int sock) {
     if(strcmp(command_type, "POST") == 0){
+        int file_size = get_file_size(file_name);
+        char size_str[BUFSIZ] = {0};
+        sprintf(size_str, "%d", file_size);
+        strncat(command, size_str, strlen(size_str));
+        print("In Post Start");
         int filefd = open(file_name, O_RDONLY);
-
         if (filefd == -1) {
             DieWithError(" open file failed") ;
         }
-
+        print("Start Sending in Post");
         if (send(sock, command, strlen(command), 0) != strlen(command))
             DieWithError("send() sent a different number of bytes than expected");
-
+        print("Finish Sending in Post");
+        print("Start Handling Post Response");
         handlePostResponse_Request(file_name, sock, filefd);
+        print("End Handling Post Response");
     } else if (strcmp(command_type, "GET") == 0){
+        print("Start Sending in GET");
         if (send(sock, command, strlen(command), 0) != strlen(command))
             DieWithError("send() sent a different number of bytes than expected");
+        print("Stop Sending in GET");
+        print("Start Handling GET Response");
         handleGetResponse(file_name, sock);
+        print("Stop Handling GET Response");
     } else{
         DieWithError("Command not supported");
     }
@@ -125,56 +150,92 @@ void startConnection(char * command, char* command_type, char* file_name, char* 
 void handleGetResponse(char* filename, int socket) {
 
     int bytesReceived = 0;
+    /*char* recvBuff = malloc(RCVBUFSIZE*sizeof(char));
+    memset(recvBuff, 0, RCVBUFSIZE);*/
     char recvBuff[RCVBUFSIZE]={0};
-    char fileSizeBuff[BUFSIZ]={0};
-
+    /*char* fileSizeBuff = malloc(BUFSIZ*sizeof(char));
+    memset(fileSizeBuff, 0, BUFSIZ);*/
+    print("Start Recieve The File Size from GET");
     /* Receiving file size */
-    recv(socket, fileSizeBuff, BUFSIZ, 0);
-    int file_size = atoi(fileSizeBuff);
+    //TODO: Add Again if No Effect
+    //recv(socket, fileSizeBuff, BUFSIZ, 0);
+    print("Stop Recieve The File Size from GET");
+
+    //int file_size = atoi(fileSizeBuff);
+    //free(fileSizeBuff);
+
+    //printf("From Socket %d and the size sent is %d\n", socket, file_size);
 
     /* Create file where data will be stored */
-    FILE *fp;
-    fp = fopen(filename, "w");
-    if(NULL == fp)
-    {
-        DieWithError("Error opening file");
-    }
+    char repoBuff[BUFSIZ] = {0};
+    /*char* repoBuff = malloc(BUFSIZ*sizeof(char));
+    memset(repoBuff, 0, BUFSIZ);*/
 
-    /* Receive data in chunks*/
-    //TODO:Here While
-    if(file_size > 0 && (bytesReceived = read(socket, recvBuff, RCVBUFSIZE)) > 0)
-    {
-        //printf("Bytes received %s\n",recvBuff);
-        //printf("The file Size : %d and bytes : %d\n",file_size, bytesReceived);
-        file_size -= bytesReceived;
-        fprintf(fp, "%s", recvBuff);
-    }
+    //Wait For OK
+    print("Wait From OK for GET");
+    if ((recv(socket, repoBuff, BUFSIZ, 0)) > 0) {
+        //printf("%s\n", repoBuff);
+        print("Check OK for POST");
+        int file_size = isOK(repoBuff);
+        if (file_size != -1) {
+            print("Create a file in GET");
+            FILE *fp;
+            fp = fopen(filename, "w");
+            if(fp == NULL)
+            {
+                DieWithError("Error opening file");
+            }
+            print("Stop Create a file in GET");
 
-    fclose(fp);
+            /* Receive data in chunks*/
+            print("While Reading the File from GET");
+            while (file_size > 0 && (bytesReceived = recv(socket, recvBuff, RCVBUFSIZE, 0)) > 0)
+            //if ((bytesReceived = recv(socket, recvBuff, RCVBUFSIZE, 0)) > 0)
+            {
+                print("************************************ REC Start");
+                //bytesReceived = recv(socket, recvBuff, RCVBUFSIZE, 0);
+                print("************************************ REC END");
+                //printf("The file Size : %d and bytes : %d\n",file_size, bytesReceived);
+                file_size -= bytesReceived;
+                //fprintf(fp, "%s", recvBuff);
+                fwrite(recvBuff, 1, bytesReceived, fp);
+            }
+            print("End Reading the File from GET");
+            //free(recvBuff);
+            fclose(fp);
 
-    if(bytesReceived < 0)
-    {
-        DieWithError("\n Read Error \n");
+            if(bytesReceived < 0)
+            {
+                DieWithError("\n Read Error \n");
+            }
+        }
     }
 }
 
 void handlePostResponse_Request(char* filename, int socket, int fifd) {
 
     char repoBuff[BUFSIZ] = {0};
+    /*char* repoBuff = malloc(BUFSIZ*sizeof(char));
+    memset(repoBuff, 0, BUFSIZ);*/
 
     //Wait For OK
-    if ((read(socket, repoBuff, BUFSIZ)) > 0)
+    print("Wait From OK for POST");
+    if ((recv(socket, repoBuff, BUFSIZ, 0)) > 0)
     {
-        printf("%s\n",repoBuff);
-        if (isOK(repoBuff) == 1) {
+        //printf("%s\n",repoBuff);
+        print("Check OK for POST");
+        if (isOK(repoBuff) != -1) {
             //Start Sending the File
+            print("OK Recieved and Start send File To server from Post");
             sendFileToServer(filename, socket, fifd);
+            print("End send File To server from Post");
         } else {
-            DieWithError(repoBuff);
+            //DieWithError(repoBuff);
         }
     } else {
         DieWithError("Error While Recieving");
     }
+    //free(repoBuff);
 }
 
 void sendFileToServer(char *filename, int socket, int filefd) {
@@ -182,36 +243,71 @@ void sendFileToServer(char *filename, int socket, int filefd) {
     struct stat file_stat;
     int sent_bytes;
 
+    print("Start get File Size to send");
     /* Get file stats */
     if (fstat(filefd, &file_stat) < 0)
     {
         DieWithError("Error in fstat");
     }
+    print("Stop get File Size to send");
 
     int fileSize = file_stat.st_size;
-    char fileSizeStr[BUFSIZ]={0};
-    sprintf(fileSizeStr, "%d", fileSize);
+    //char fileSizeStr[BUFSIZ]={0};
+    /*char* fileSizeStr = malloc(BUFSIZ*sizeof(char));
+    memset(fileSizeStr, 0, BUFSIZ);*/
+    //sprintf(fileSizeStr, "%d\r", fileSize);
 
+    /*print("Start Send the File Size");
     if(send(socket, fileSizeStr, sizeof(fileSizeStr), 0) < 0) {
         DieWithError("Error while sending size file");
     }
+    print("Stop Send the File Size");*/
+    //free(fileSizeStr);
 
     /* Sending file data */
+    print("Start Send the File in Post");
     sent_bytes = sendfile(socket, filefd, NULL, fileSize);
-    if (sent_bytes < fileSize) {
+    print("Stop Send the File in Post");
+    if (sent_bytes <= 0) {
         DieWithError("Error While Sending file");
     }
 }
 
 int isOK(char* msg) {
-    char *httpType = strtok(msg, " ");
+    //printf("The Message is : %s\n", msg);
+    char * first_msg = strtok(msg, "\n");
+    char * sec_msg = strtok(NULL, "\n");
+    printf("%s\n", first_msg);
+    //printf("Second is : %s\n", sec_msg);
+    strtok(first_msg, " ");
     char *respType = strtok(NULL, " ");
-
     if (strcmp(respType, "200") == 0) {
-        return 1;
+        return atoi(sec_msg);
     } else if (strcmp(respType, "404") == 0) {
-        return 0;
+        return -1;
     }
+    return -1;
+}
+
+void print(char * str) {
+    //printf("%s\n", str);
+}
+
+int get_file_size(char * filename) {
+    long file_size;
+    FILE * file_to_send;
+
+    /* Opening File to read its contents */
+    file_to_send = fopen(filename, "r");
+    if (file_to_send == NULL)
+    {
+        DieWithError("Faild to Open File\n");
+    }
+
+    /* Sizing The file first */
+    fseek(file_to_send, 0L, SEEK_END);
+    file_size = ftell(file_to_send);
+    return file_size;
 }
 
 //char ** parse_command (char * command){
--- Basic_Socket_Programming/Server_Side/HTTPServer.c
+++ Basic_Socket_Programming/Server_Side/HTTPServer.c
@@ -1,19 +1,22 @@
+#include <stdio.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
 #include "Die_with_error.c"
 
-#define CLIENTS_IN_QUEUE 100
-#define RCVBUFSIZE 1024
+#define CLIENTS_IN_QUEUE 10
+#define RCVBUFSIZE 10240
 
 void responseForClient(int sckt);
-void sendMessageToClient(int socket, int code);
-void postResponse(int sckt, char* filename);
+void sendMessageToClient(int socket, int code, int file_size);
+void postResponse(int sckt, char *filename, int file_size);
+void print(char * str);
 void getResponse(int sckt, char* filename, char* fileType);
-void sendBytes(int sckt, char *filename, char* buffer, FILE *file_to_send);
-void sendText(int sckt, char *filename, char* buffer, FILE *file_to_send);
-
+void sendBytes(int sckt, int fileSize, int filed);
 /* TCP client handling function */
 int main(int argc, char *argv[]) {
 
@@ -32,12 +35,13 @@ int main(int argc, char *argv[]) {
     }
 
     /*Main Thread Setup*/
-
     servPort = atoi(argv[1]);   /* First arg: local port */
 
     /* Create socket for incoming connections */
+    print("Create The Socket");
     if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
         DieWithError("socket () failed");
+    print("End Create The Socket");
 
     /* Construct local address structure */
     memset(&servAddr, 0, sizeof(servAddr));
@@ -46,25 +50,37 @@ int main(int argc, char *argv[]) {
     servAddr.sin_port = htons(servPort);
 
     /* Bind to the local address */
+    print("Bind The Socket");
     if (bind(servSock, (struct sockaddr *)&servAddr,sizeof(servAddr)) < 0)
         DieWithError("bind () failed");
+    print("End Bind The Socket");
 
     /* Mark the socket so it will listen for incoming connections */
+    print("Listen The Socket");
     if (listen(servSock, CLIENTS_IN_QUEUE) < 0)
         DieWithError("listen() failed") ;
+    print("End Listen The Socket");
 
     for (;;) /* Run forever */
     {
         /* Set the size of the in-out parameter */
         clntLen = sizeof(clntAddr);
         /* Wait for a client to connect */
-        if ((clntSock = accept(servSock, (struct sockaddr *) &clntAddr, &clntLen)) < 0)
+        print("Accept The Socket");
+        if ((clntSock = accept(servSock, (struct sockaddr *) &clntAddr, &clntLen)) < 0) {
             DieWithError("accept() failed");
+        }
+        print("End Accept The Socket");
 
         int thrd_num = fork();
         if (thrd_num == 0) {
+            print("Child created");
             //Now in the Child Process
+            print("Start Responding");
             responseForClient(clntSock);
+            print("End Responding");
+            close(clntSock);
+            clntSock = -1;
             exit(0);
         } else if (thrd_num > 0) {
             //Now in the Parent Process
@@ -73,105 +89,132 @@ int main(int argc, char *argv[]) {
             perror("Error Creating the Child");
         }
     }
-
     close(servSock);  /* Close socket */
 }
 
 void responseForClient(int sckt) {
 
     char rcvBuffer[RCVBUFSIZE]={0};
+    /*char* rcvBuffer = malloc(RCVBUFSIZE*sizeof(char));
+    memset(rcvBuffer, 0, RCVBUFSIZE);*/
+
     int recvMsgSize;
     /* Receive message from client */
+    print("********************** Receving Command From Client");
     if ((recvMsgSize = recv(sckt, rcvBuffer, RCVBUFSIZE, 0)) < 0)
         DieWithError("recv() failed");
+    print("********************** End Receving Command From Client");
 
     /* Send received string and receive again until end of transmission */
+    print("Before Start While");
     while (recvMsgSize > 0) /* zero indicates end of transmission */
     {
         //An Empty Message
         if (strlen(rcvBuffer) == 0) {
-            sendMessageToClient(sckt, 404);
+            sendMessageToClient(sckt, 404, 0);
         } else {
-            printf("%s\n", rcvBuffer);
-            char * msgType = strtok(rcvBuffer, " ");
+            char * first_msg = strtok(rcvBuffer, "\n");
+            char * sec_msg = strtok(NULL, "\n");
+            printf("%s\n", first_msg);
+            char * msgType = strtok(first_msg, " ");
             char *filename = strtok(NULL, " ");
-            char *hostname = strtok(NULL, " ");
-            char fileNameTemp[100]={0};
+            strtok(NULL, " ");
+            char fileNameTemp[BUFSIZ]={0};
             strcpy(fileNameTemp, filename);
-
             strtok(filename, ".");
             char *fileType = strtok(NULL, ".");
 
+            print("Check Extentiosn");
             if (!strcmp(fileType, "html") && !strcmp(fileType, "jpg") && !strcmp(fileType, "txt")) {
-                sendMessageToClient(sckt, 404);
-                return;
+                sendMessageToClient(sckt, 404, 0);
+                //return;
             }
+            print("After Check Extentiosn");
 
             if(strncmp(msgType, "GET", 3) == 0) {
-                //********************
-                //TODO:Delete it
-                //This Code only for test Get Client
-                send(sckt,"27", sizeof("27"), 0);
-                sendMessageToClient(sckt, 404);
-                //******************
-                //TODO:GetResponse
                 getResponse(sckt,fileNameTemp,fileType);
             } else if(strncmp(msgType, "POST", 4) == 0) {
-                postResponse(sckt, fileNameTemp);
+                print("Start Sending For Post : THe File");
+                postResponse(sckt, fileNameTemp, atoi(sec_msg));
+                print("End Sending For Post : THe File");
             }
         }
 
         //Keep Reading from the user
+        print("Start Keep Receving");
         recvMsgSize = recv(sckt, rcvBuffer, sizeof(rcvBuffer), 0);
+        print("After Start Keep Receving");
     }
-    close(sckt);
+    print("After Start While");
+    //free(rcvBuffer);
 }
 
-void sendMessageToClient(int sckt, int code) {
+void sendMessageToClient(int sckt, int code, int file_size) {
 
-    char *msg;
+    char msg[BUFSIZ] = {0};
 
+    char size_str[BUFSIZ] = {0};
     switch (code) {
         case 200:
-            msg = "HTTP/1.0 200 OK\\r\\n\r";
+            //size_str[0] = (char) file_size;
+            //printf("The size %s\n", size_str);
+            sprintf(size_str, "%d", file_size);
+            strncpy(msg, "HTTP/1.0 200 OK\\r\\n\r\n", sizeof("HTTP/1.0 200 OK\\r\\n\r\n"));
+            strncat(msg, size_str, strlen(size_str));
             send(sckt,msg, strlen(msg), 0);
             break;
         case 404:
-            msg = "HTTP/1.0 404 Not Found\\r\\n\r";
+            strncpy(msg, "HTTP/1.0 404 Not Found\\r\\n\r\n", sizeof("HTTP/1.0 404 Not Found\\r\\n\r\n"));
             send(sckt,msg, strlen(msg), 0);
             break;
     }
 }
 
-void postResponse(int sckt, char* filename) {
-    char buffer[BUFSIZ]={0};
-    char fileBuffer[RCVBUFSIZE]={0};
-    int file_size;
+void postResponse(int sckt, char *filename, int file_size) {
+    char buffer[BUFSIZ] = {0};
+    /*char* buffer = malloc(BUFSIZ*sizeof(char));
+    memset(buffer, 0, BUFSIZ);*/
+    char fileBuffer[RCVBUFSIZE] = {0};
+    /*char* fileBuffer = malloc(RCVBUFSIZE*sizeof(char));
+    memset(fileBuffer, 0, RCVBUFSIZE);*/
+    //int file_size;
     int bytesReceived = 0;
     FILE *received_file;
 
-    sendMessageToClient(sckt, 200);
+    print("Start Sending the OK message for Post");
+    sendMessageToClient(sckt, 200, 0);
+    print("End Sending the OK message for Post");
 
     /* Receiving file size */
-    recv(sckt, buffer, BUFSIZ, 0);
-    file_size = atoi(buffer);
+    print("Start Receving The File Size");
+    //recv(sckt, buffer, BUFSIZ, 0);
+    print("End Receving The File Size");
+    //file_size = atoi(buffer);
+    //free(buffer);
 
+    print("Start Create The Receving File");
     received_file = fopen(filename, "w");
 
     if (received_file == NULL)
     {
         DieWithError("Faild to Open File\n");
     }
+    print("The Receving File Created");
 
     /* Receive data in chunks*/
-    //TODO:HEre While
-    if(file_size > 0 && (bytesReceived = read(sckt, fileBuffer, RCVBUFSIZE)) > 0)
+    print("Start Receving the File from Post");
+    while (file_size > 0 && (bytesReceived = recv(sckt, fileBuffer, RCVBUFSIZE, 0)) > 0)
     {
         //printf("Bytes received %s\n",fileBuffer);
         //printf("The file Size : %d and bytes : %d\n",file_size, bytesReceived);
+        print("******************* Start REc");
+        print("******************* End REc");
         file_size -= bytesReceived;
-        fprintf(received_file, "%s", fileBuffer);
+        //fprintf(received_file, "%s", fileBuffer);
+        fwrite(fileBuffer, 1, bytesReceived, received_file);
     }
+    print("End Receving the File from Post");
+    //free(fileBuffer);
 
     fclose(received_file);
 
@@ -189,20 +232,15 @@ void postResponse(int sckt, char* filename) {
     fclose(received_file);*/
 }
 
-/*do {
-            if ((pid = waitpid(pid, &status, WNOHANG)) == -1)
-                perror("wait() error");
-            else {
-                if (WIFEXITED(status))
-                    printf("child exited with status of %d\n", WEXITSTATUS(status));
-                else puts("child did not exit successfully");
-            }
-        } while (pid == 0);*/
-
+void print(char * str) {
+    //printf("%s\n", str);
+}
 
 void getResponse(int sckt, char* filename, char* fileType){
+
+    //printf("The File Name : %s\n", filename);
+
     if(access(filename, R_OK) != -1){
-        sendMessageToClient(sckt,200);
 
         /* buffer to read from the file and send data to socket */
         char buffer[BUFSIZ]={0};
@@ -219,52 +257,66 @@ void getResponse(int sckt, char* filename, char* fileType){
         /* Sizing The file first */
         fseek(file_to_send, 0L, SEEK_END);
         file_size = ftell(file_to_send);
+        sendMessageToClient(sckt, 200, file_size);
         rewind(file_to_send);
         if (file_size < 1) {
             printf("File is Empty");
             send(sckt , "NULL", sizeof("NULL") , 0);
-            return;
+            //return;
         }
 
         /* Choosing the method of transmit */
-        if(!strcmp(fileType,"txt") || !strcmp(fileType,"html")){
-            sendText(sckt, filename,buffer , file_to_send);
-        } else {
-            sendBytes(sckt, filename,buffer, NULL);
-        }
+        //printf("The File Name 2  : %s\n", filename);
+        int filefd = open(filename, O_RDONLY);
+        sendBytes(sckt, file_size, filefd);
+        close(filefd);
     } else {
-        sendMessageToClient(sckt,404);
+        sendMessageToClient(sckt, 404, 0);
         DieWithError("File Doesn't Exist \n ");
     }
 }
 
-void sendBytes(int sckt, char *filename, char* buffer, FILE *file_to_send) {
-    bzero(buffer,BUFSIZ);
-    size_t chunk_size ;
-    while((chunk_size = fread(buffer, sizeof(char), BUFSIZ, file_to_send)) > 0)
+void sendBytes(int sckt, int fileSize, int filed) {
+
+    //struct stat file_stat;
+    int sent_bytes;
+    /*
+
+    print("Start get File Size to send");
+    if (fstat(filed, &file_stat) < 0)
     {
-        if(send(sckt, buffer, chunk_size , 0) < 0)
-        {
-            printf("Failed to send a chunk of size %ld \n ", chunk_size);
-            break;
-        }
-        bzero(buffer, BUFSIZ);
+        DieWithError("Error in fstat");
     }
+    print("Stop get File Size to send");
 
-    fclose(file_to_send);
-    printf("Ok File %s from Client was Sent!\n", filename);
+    int fileSize = file_stat.st_size;*/
+    //char fileSizeStr[BUFSIZ]={0};
+    /*char* fileSizeStr = malloc(BUFSIZ*sizeof(char));
+    memset(fileSizeStr, 0, BUFSIZ);*/
+    //sprintf(fileSizeStr, "%d\r", fileSize);
+
+    /*print("Start Send the File Size");
+    if(send(socket, fileSizeStr, sizeof(fileSizeStr), 0) < 0) {
+        DieWithError("Error while sending size file");
+    }
+    print("Stop Send the File Size");*/
+    //free(fileSizeStr);
+
+    /* Sending file data */
+    print("Start Send the File in Post");
+    sent_bytes = sendfile(sckt, filed, NULL, fileSize);
+    print("Stop Send the File in Post");
+    if (sent_bytes <= 0) {
+        DieWithError("Error While Sending file");
+    }
 }
 
-void sendText(int sckt, char *filename, char* buffer, FILE *file_to_send) {
-    bzero(buffer,BUFSIZ);
-    size_t chunk_size ;
-    while (fgets(buffer, BUFSIZ , file_to_send) != NULL){
-        if(send(sckt, buffer, BUFSIZ , 0) < 0)
-        {
-            printf("Failed to send a chunk of size %d \n ", BUFSIZ);
-            break;
-        }
-    }
-    fclose(file_to_send);
-    printf("Ok File %s from Client was Sent!\n", filename);
-}
+/*do {
+            if ((pid = waitpid(pid, &status, WNOHANG)) == -1)
+                perror("wait() error");
+            else {
+                if (WIFEXITED(status))
+                    printf("child exited with status of %d\n", WEXITSTATUS(status));
+                else puts("child did not exit successfully");
+            }
+        } while (pid == 0);*/
